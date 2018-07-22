f3ce23cc6f9913d1dd65f70c17bc2d4d2aec0cc4
elaiyarasan <milayarasan@gmail.com>
2018-07-22 13:52:54 +0530

    Update sum_of_n_numbers.c

--- c/sum_of_n_numbers.c
+++ c/sum_of_n_numbers.c
@@ -1,1 +1,17 @@
+//Sum of n numbers
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+int n,c,sum=0,value;
+    scanf("%d",&n);
+    for(c=1;c<=n;c++)
+    {
+        sum=sum+c;
+    }
+    printf("%d",sum);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
