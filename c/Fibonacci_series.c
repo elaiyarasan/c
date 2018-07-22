e7c10d8df616e3694a44e2e84047eb778108da17
elaiyarasan <milayarasan@gmail.com>
2018-07-22 13:52:15 +0530

    Update Fibonacci_series.c

--- c/Fibonacci_series.c
+++ c/Fibonacci_series.c
@@ -17,20 +17,3 @@ int c,n,fact=1;
     return 0;
 }
 
-Sum of n numbers
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-int main() {
-int n,c,sum=0,value;
-    scanf("%d",&n);
-    for(c=1;c<=n;c++)
-    {
-        sum=sum+c;
-    }
-    printf("%d",sum);
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-    return 0;
-}
